5e65f837c44708f216a2e0a67b7553e857076eb5
vegoria <vegoria@gmail.com>
2016-11-24 12:19:44 +0100

    Prepare interface for goal function and problem class

---
+++ GeneticAlgorithm/GeneticAlgorithm/Problem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    class Problem
+    {
+        private Population population;
+        private ICrossing corssing;
+        private IMutation mutation;
+        private ISelection selection;
+        private IGoalFunction goalFunction;
+    }
+}
