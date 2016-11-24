3695b94b3612fd1183dfee8d92fea8e38440b417
vegoria <vegoria@gmail.com>
2016-11-24 12:12:51 +0100

    Added necessary fields to Population

--- GeneticAlgorithm/GeneticAlgorithm/Population.cs
+++ GeneticAlgorithm/GeneticAlgorithm/Population.cs
@@ -8,5 +8,29 @@ namespace GeneticAlgorithm
 {
     public class Population
     {
+        private static int nextGenerationId = 1;
+        private readonly int populationSize;
+        private readonly int populationId;
+        public Population(int size)
+        {
+            populationSize = size;
+            populationId = nextGenerationId++;
+        }
+
+        public int PopulationSize
+        {
+            get
+            {
+                return populationSize;
+            }
+        }
+
+        public int PopulationId
+        {
+            get
+            {
+                return populationId;
+            }
+        }
     }
 }
